31e29b4e94d729132296c1411632fad908d0d201
72347565 <72347565@trilce.edu.pe>
2025-07-15 17:36:46 +0000

    Resolver datos de la caja

--- modules/Pos/Http/Controllers/CashController.php
+++ modules/Pos/Http/Controllers/CashController.php
@@ -258,7 +258,7 @@ class CashController extends Controller
                         $query->where('cash_id', $cash_id);
                     })
                     ->sum('payment');
-                if ($cash_document->sale_note->related) {
+                if ($cash_document->sale_note->related->count()) {
                         foreach ($cash_document->sale_note->related as $sale_note) {
                             if($sale_note){
                                $sale = SaleNote::where('id', $sale_note)->first(); 
