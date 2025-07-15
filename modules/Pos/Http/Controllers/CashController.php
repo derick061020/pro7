a290a0a9dc2fa8ee4656b9f17b846fa7d2bbc549
72347565 <72347565@trilce.edu.pe>
2025-07-15 14:58:31 +0000

    test

--- modules/Pos/Http/Controllers/CashController.php
+++ modules/Pos/Http/Controllers/CashController.php
@@ -1064,8 +1064,6 @@ class CashController extends Controller
      * @throws \Throwable
      */
     public function reportA4($cash) {
-        echo(json_encode(Cash::findOrFail($cash)->cash_documents));
-        exit();
         $temp = tempnam(sys_get_temp_dir(), 'cash_pdf_a4');
         file_put_contents($temp, $this->getPdf($cash, 'a4'));
 
