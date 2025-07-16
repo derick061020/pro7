2b4f79931c6c1d7a7cd16d8394de205ef2e79747
72347565 <72347565@trilce.edu.pe>
2025-07-16 00:18:32 +0000

    Hola

--- modules/Pos/Http/Controllers/CashController.php
+++ modules/Pos/Http/Controllers/CashController.php
@@ -259,17 +259,14 @@ class CashController extends Controller
                     })
                     ->sum('payment');
                 $ale = SaleNote::where('id', $sale_note->id)->first();
-                var_dump($ale->related);
                 if ($sale_note->related != null) {
                         foreach ($sale_note->related as $related) {
                             if($related){
                                $sale = SaleNote::where('id', $related)->first(); 
-                               die(json_encode($sale));
                                if($sale && strtotime($sale->date_of_issue . ' ' . $sale->time_of_issue) <= strtotime($cash->date_opening.' '.$cash->time_opening)){
                                 $totalPayments -= ($sale->currency_type_id == 'PEN') 
                                    ? $sale->total 
                                    : ($sale->total * $sale->exchange_rate_sale);
-                                   echo($totalPayments);
                                }
                             }
                         }
